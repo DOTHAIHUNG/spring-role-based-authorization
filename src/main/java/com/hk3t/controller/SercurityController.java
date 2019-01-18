9241db5db8a9831a56975c7abd5d310d262c0817
HUNGDT <hk2t.thaihung@gmail.com>
2019-01-18 08:43:49 +0700

    done

---
+++ src/main/java/com/hk3t/controller/SercurityController.java
@@ -0,0 +1,49 @@
+package com.hk3t.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class SercurityController {
+
+    private String getPrincipal() {
+        String userName = null;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            userName = ((UserDetails) principal).getUsername();
+        } else {
+            userName = principal.toString();
+        }
+        return userName;
+    }
+
+    @RequestMapping(value = {"/", "/home"})
+    public String homepage(Model model) {
+        model.addAttribute("user", getPrincipal());
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap modelMap) {
+        modelMap.addAttribute("user", getPrincipal());
+        return "admin";
+    }
+
+    @RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+    public String accessDeniedPage(Model model) {
+        model.addAttribute("user", getPrincipal());
+        return "accessDenied";
+    }
+
+    @RequestMapping(value = "/dba", method = RequestMethod.GET)
+    public String dbaPage(Model model) {
+        model.addAttribute("user", getPrincipal());
+        return "dba";
+    }
+}
